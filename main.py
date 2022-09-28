8e0342adf373429e74f650d7268a674d81b4ae48
Alicia <95257458+Aguerrerosipac@users.noreply.github.com>
2022-09-28 15:01:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("HOLA MUNDO")
+opcion= input("Ingrese la operacion aritmetica: ")
+var1= float (input("Escribe el primer valor: "))
+var2= float (input("Escribe el segundo valor: "))
+0
+if opcion== "suma":
+    resultado_suma = var1 + var2
+    print(resultado_suma)
+if opcion== "resta":
+    resultado_resta= var1 - var2
+    print(resultado_resta)
+if opcion == "multiplicacion":
+    resultado_multiplicacion= var1 * var2
+    print(resultado_multiplicacion)
+if opcion == "division":
+    if var1 % var2 == 0:
+        resultado_division= var1 / var2
+        print("igual: ", resultado_division)
